292a5c3af445041b5ee8467f0d47aeae31b883a6
Skyswimsky <Skyswim07@gmail.com>
2021-02-27 19:49:10 +0100

    Added some basic structure for the future CharacterSheet stuff.
    The Controllers(under XF\Pub\Controller) basically do backend shenanigans for stuff.
    CharacterSheet itself is a new Controller for the future CharacterSheet, currently not doing much.
    The Member.php is an extension to the Member Controller of Xenforo, which basically has all controlling logic for the profile.
    The character_sheet_view_template is the template to do the looks at the character sheets. I made it manually inside the output folder.
    
    Naming conventions are basically to mirror things how they are for the original XenForo.

---
+++ XF/Pub/Controller/CharacterSheet.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Terrasphere\Charactermanager\XF\Pub\Conntroller;
+
+//Controller for the CharacterSheet php page.
+
+class CharacterSheet extends \XF\Pub\Controller\AbstractController
+{
+
+}
