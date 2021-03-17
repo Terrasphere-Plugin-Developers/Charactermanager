45d51c67102ab8b6334752ee83313a399894023f
Fluffies <Azuremeadow@gmail.com>
2021-03-17 03:23:38 -0400

    Added route for mastery shop.

--- Pub/Controller/CharacterSheet.php
+++ Pub/Controller/CharacterSheet.php
@@ -1,7 +1,9 @@
 <?php
 
-namespace Terrasphere\Charactermanager\Pub\Conntroller;
+namespace Terrasphere\Charactermanager\Pub\Controller;
 
+use XF\Entity\User;
+use XF\Mvc\ParameterBag;
 use XF\Pub\Controller\AbstractController;
 
 class CharacterSheet extends AbstractController
@@ -9,5 +11,10 @@ class CharacterSheet extends AbstractController
     public function actionIndex(): \XF\Mvc\Reply\Redirect
     {
         return $this->redirect($this->buildLink('terrasphere-core/masteries/mastery-list'));
+    }
+
+    public function actionSelectnew(ParameterBag $params)
+    {
+
     }
 }
