c202021063f48ff487831107e231eb9f6f03ee18
Fluffies <Azuremeadow@gmail.com>
2021-04-11 02:08:49 -0400

    Upgrading masteries now works. Fixed numerous glitches. Everything in the mastery shops should update except the player's currencies on the top of the profile.

--- XF/Pub/Controller/Member.php
+++ XF/Pub/Controller/Member.php
@@ -4,11 +4,9 @@
 
 namespace Terrasphere\Charactermanager\XF\Pub\Controller;
 
-use Laminas\Mail\Header\From;
 use Terrasphere\Charactermanager\Entity\CharacterMastery;
+use Terrasphere\Core\Entity\Rank;
 use XF\Entity\User;
-use XF\Entity\UserProfile;
-use XF\Mvc\FormAction;
 use XF\Mvc\ParameterBag;
 
 class Member extends XFCP_Member
@@ -21,12 +19,15 @@ class Member extends XFCP_Member
 
         $masterySlots = CharacterMastery::getCharacterMasterySlots($this, $params->user_id);
 
+        $maxRank = Rank::maxRank($this);
+
 		$viewParams = [
 		    'user' => $user,
 		    'masterySlots' => $masterySlots,
+		    'maxRank' => $maxRank['rank_id'],
             'canViewCS' => $this->canVisitorViewCharacterSheet($params->user_id),
             'canViewRevisions' => $this->canVisitorViewRevisions($params->user_id),
-            'viewerIsUser' => $this->visitorIsUser($params->user_id),
+            'canBuyMasteries' => $this->canVisitorBuyMasteries($params->user_id),
             'fourthSlotUnlocked' => $this->fourthSlotUnlocked($params->user_id),
             'fifthSlotUnlocked' => $this->fifthSlotUnlocked($params->user_id),
         ];
@@ -113,11 +114,11 @@ class Member extends XFCP_Member
                 ->with('Mastery')
                 ->where('user_id', $params['user_id'])
                 ->where('Mastery.mastery_type_id', $mastery['mastery_type_id'])
-                ->fetch();
+                ->total();
 
             // Mastery type limit check
-            if(count($sameTypeMasteries) >= $mastery->MasteryType->cap_per_character)
-                return $this->error('You already have the maximum amount of ' . $mastery->MasteryType->name . ' masteries.');
+            if($sameTypeMasteries >= $mastery->MasteryType->cap_per_character)
+                return $this->error('You already have the maximum amount of ' . $mastery->MasteryType->name . ' masteries. ('.$sameTypeMasteries.' vs '.$mastery->MasteryType->cap_per_character.')');
 
             // 4th slot restriction check
             if($params['target_index'] == 3 && !$this->fourthSlotUnlocked($params['user_id']))
@@ -132,7 +133,7 @@ class Member extends XFCP_Member
             $slotEntity['mastery_id'] = $mastery['mastery_id'];
             $slotEntity['user_id'] = $params['user_id'];
             $slotEntity['target_index'] = $params['target_index'];
-            $this->saveMasterySlot($slotEntity, $input['mastery'])->run();
+            $this->saveMasterySlot($slotEntity)->run();
 
             // Close overlay via redirect and setup AJAX parameters.
             $redirect = $this->redirect($this->buildLink('members', null, ['user_id' => $params['user_id']]));
@@ -164,41 +165,51 @@ class Member extends XFCP_Member
      */
     public function actionConfirmUpgrade(ParameterBag $params)
     {
+        // Get character's mastery instance.
         $mastery = $this->finder('Terrasphere\Charactermanager:CharacterMastery')
             ->with('Mastery')
             ->where('user_id', $params['user_id'])
             ->where('target_index', $params['target_index'])
             ->fetchOne();
 
+        // Get the current rank.
         $thisRank = $this->finder('Terrasphere\Core:Rank')
             ->where('rank_id', $mastery['rank_id'])
             ->fetchOne();
 
+        // Rank exists check.
         if($thisRank == null)
             return $this->error('Nullrankerror.');
 
+        // Get the mastery type for the ranking schema.
         $masteryType = $this->finder('Terrasphere\Core:MasteryType')
             ->where('mastery_type_id', $mastery->Mastery->mastery_type_id)
             ->fetchOne();
 
+        // Type exists check.
         if($masteryType == null)
             return $this->error('Mastery type missing error. Mastery: ' . $mastery['display_name'] . '.');
 
+        // Get the next-highest rank.
         $nextRank = $this->finder('Terrasphere\Core:Rank')
             ->where('tier', $thisRank['tier']+1)
             ->fetchOne();
 
+        // Next-highest rank check.
         if($nextRank == null)
             return $this->error('Already at max rank.');
 
+        // Get rank schema (and the currency associated with it).
         $rankSchema = $this->finder('Terrasphere\Core:RankSchema')
             ->with('Currency')
             ->where('rank_schema_id', $masteryType['rank_schema_id'])
             ->fetchOne();
 
+        // Rank schema exists check.
         if($rankSchema == null)
             return $this->error('No rank schema associated with this mastery type.');
 
+        // Temporary variable to get the cost of the next rank.
         $nxt = $this->finder('Terrasphere\Core:RankSchemaMap')
             ->where('rank_schema_id', $masteryType['rank_schema_id'])
             ->where('rank_id', $nextRank['rank_id'])
@@ -209,15 +220,23 @@ class Member extends XFCP_Member
 
         $nextCost = $nxt['cost'];
         $user = $this->finder('XF:User')->where('user_id', $params['user_id'])->fetchOne();
+        $userVal = $rankSchema->Currency->getValueFromUser($user, false);
+
+        $mastery_slot = $this->em()->create("Terrasphere\Charactermanager:CharacterMastery");
+        $mastery_slot['user_id'] = $params['user_id'];
+        $mastery_slot['target_index'] = $params['target_index'];
+
 
         $viewparams = [
             'thisRank' => $thisRank,
             'nextRank' => $nextRank,
-            'nextCost' => $nextCost,
             'rankSchema' => $rankSchema,
             'mastery' => $mastery->Mastery,
             'user' => $user,
-            'userVal' => $user[$rankSchema->Currency->user_id_column],
+            'userVal' => $rankSchema->Currency->getFormattedValue($userVal),
+            'nextCost' => $rankSchema->Currency->getFormattedValue($nextCost),
+            'afterVal' => $rankSchema->Currency->getFormattedValue($userVal - $nextCost),
+            'mastery_slot' => $mastery_slot,
         ];
         return $this->view('Terrasphere\Charactermanager:MasteryUpgradeConfirm', 'terrasphere_cm_confirm_mastery_upgrade', $viewparams);
     }
@@ -270,7 +289,33 @@ class Member extends XFCP_Member
         $nextCost = $nxt['cost'];
         $user = $this->finder('XF:User')->where('user_id', $params['user_id'])->fetchOne();
 
-        // TODO - Upgrade and AJAX ping.
+        $userVal = $rankSchema->Currency->getValueFromUser($user, false);
+
+        if($nextCost > $userVal)
+            return $this->error('Error: Insufficient Funds.');
+
+        if($this->adjustCurrency($user, $nextCost, "Mastery Upgrade (".$mastery->Mastery->display_name." Rank ".$nextRank['name'].")", $rankSchema->Currency->currency_id))
+        {
+            $fourthSlotUnlockedPrev = $this->fourthSlotUnlocked($params['user_id']);
+            $fifthSlotUnlockedPrev = $this->fifthSlotUnlocked($params['user_id']);
+
+            // Update mastery entity to next rank.
+            $mastery->fastUpdate('rank_id', $nextRank['rank_id']);
+
+            // Close overlay via redirect and setup AJAX parameters.
+            $redirect = $this->redirect($this->buildLink('members', null, ['user_id' => $params['user_id']]));
+            $redirect->setJsonParam('masterySlotIndex', $params['target_index']);
+            $redirect->setJsonParam('newRankTier', $nextRank['tier']);
+            $redirect->setJsonParam('max', $nextRank['tier'] == Rank::maxTier($this));
+            $redirect->setJsonParam('fourthSlotWasUnlocked', $fourthSlotUnlockedPrev);
+            $redirect->setJsonParam('fifthSlotWasUnlocked', $fifthSlotUnlockedPrev);
+            $redirect->setJsonParam('fourthSlotUnlocked', $this->fourthSlotUnlocked($params['user_id']));
+            $redirect->setJsonParam('fifthSlotUnlocked', $this->fifthSlotUnlocked($params['user_id']));
+
+            return $redirect;
+        }
+
+        return $this->error("Currency adjustment error...");
     }
 
 	public function canVisitorViewCharacterSheet(int $userID): bool {
@@ -286,14 +331,19 @@ class Member extends XFCP_Member
         return $this->visitorIsUser($userID); // TODO || Edit permissions!
     }
 
+    public function canVisitorBuyMasteries(int $userID): bool {
+        return $this->visitorIsUser($userID);
+    }
+
     private function visitorIsUser(int $userID): bool {
         return \XF::visitor()->user_id == $userID;
     }
 
     private function fourthSlotUnlocked(int $userID): bool {
         $cAndHigherMasteries = $this->finder('Terrasphere\Charactermanager:CharacterMastery')
+            ->with('Rank')
             ->where('user_id', $userID)
-            ->where('rank_id', '>=', 2)
+            ->where('Rank.tier', '>', 1)
             ->total();
 
         return $cAndHigherMasteries >= 3;
@@ -301,9 +351,9 @@ class Member extends XFCP_Member
 
     private function fifthSlotUnlocked(int $userID): bool {
         return $this->finder('Terrasphere\Charactermanager:CharacterMastery')
-            ->with('Mastery')
+            ->with('Rank')
             ->where('user_id', $userID)
-            ->where('rank_id', '>=', 5)
+            ->where('Rank.tier', '>', 4)
             ->fetchOne() != null;
     }
 
@@ -353,5 +403,51 @@ class Member extends XFCP_Member
             if(!$match) $ret[$mastery['mastery_id']] = $mastery;
         }
         return $ret;
+    }
+
+    private function adjustCurrency(User $user, int $cost, string $message, $currencyID)
+    {
+        $this->assertPostOnly();
+
+        /** @var \DBTech\Credits\Entity\Currency $currency */
+        $currency = $this->assertRecordExists('DBTech\Credits:Currency', $currencyID, null, null);
+
+        $input = [
+            'username' => $user['username'],
+            'amount' => $cost,
+            'message' => $message,
+            'negate' => true
+        ];
+
+        /** @var \DBTech\Credits\EventTrigger\Adjust $adjustEvent */
+        $adjustEvent = $this->repository('DBTech\Credits:EventTrigger')->getHandler('adjust');
+        if (!$adjustEvent->isActive())
+        {
+            return $this->error(\XF::phrase('dbtech_credits_invalid_eventtrigger'));
+        }
+
+        // Make sure this is set
+        $currency->verifyAdjustEvent();
+
+        $message = $input['message'];
+        $multiplier = $input['negate'] ? (-1 * $input['amount']) : $input['amount'];
+
+        // First test add or remove credits
+        $adjustEvent->testApply([
+            'currency_id' => $currency->currency_id,
+            'multiplier' => $multiplier,
+            'message' => $message,
+            'source_user_id' => $user->user_id,
+        ], $user);
+
+        // Then properly add or remove credits
+        $adjustEvent->apply($user->user_id, [
+            'currency_id' => $currency->currency_id,
+            'multiplier' => $multiplier,
+            'message' => $message,
+            'source_user_id' => $user->user_id,
+        ], $user);
+
+        return true;
     }
 }
---
+++ _files/js/Terrasphere/Charactermanager/upgrade_mastery.js
@@ -0,0 +1,87 @@
+(function($) {
+    $.fn.changeElementType = function(newType) {
+        var attrs = {};
+
+        $.each(this[0].attributes, function(idx, attr) {
+            attrs[attr.nodeName] = attr.nodeValue;
+        });
+
+        this.replaceWith(function() {
+            return $("<" + newType + "/>", attrs).append($(this).contents());
+        });
+    }
+})(jQuery);
+
+!function($, window, document, _undefined)
+{
+    "use strict";
+
+    XF.TS_CM_UpgradeMastery = XF.Element.newHandler({
+
+        options: {},
+
+        init: function()
+        {
+            var $form = this.$target;
+
+            $form.on('ajax-submit:response', $.proxy(this, 'ajaxResponse'));
+        },
+
+        ajaxResponse: function(e, data)
+        {
+            // Set up the new mastery's display...
+            var lastRankClass = "rank-"+(data.newRankTier-1);
+            var newRankClass = "rank-"+(data.newRankTier);
+            $("#mastery-container-" + data.masterySlotIndex + " ." + lastRankClass).removeClass(lastRankClass).addClass(newRankClass);
+
+            // Reset flashy animation element by clone+delete.
+            var elem = $("#mastery-container-" + data.masterySlotIndex + " .mastery-change-flash");
+            var copy = elem.clone(true);
+            elem.before(copy);
+            $("#mastery-container-" + data.masterySlotIndex + " .mastery-change-flash:last-child").remove();
+
+            // Play reset animation.
+            copy.css("animation-play-state", "running");
+
+            // If the fourth slot was unlocked by this upgrade...
+            if(!data.fourthSlotWasUnlocked && data.fourthSlotUnlocked)
+            {
+                // Hide the locked element of the previously locked slot.
+                $("#mastery-container-3" + " .character-sheet-mastery-lock").hide();
+
+                // Show the empty 'select new' portion of the slot.
+                $("#mastery-container-3" + " .character-sheet-mastery-empty").show();
+
+                // ...also play the flashy animation thingy.
+                $("#mastery-container-3" + " .mastery-change-flash").css("animation-play-state", "running");
+            }
+
+            // If the fifth slot was unlocked by this upgrade...
+            if(!data.fifthSlotWasUnlocked && data.fifthSlotUnlocked)
+            {
+                // Hide the locked element of the previously locked slot.
+                $("#mastery-container-4" + " .character-sheet-mastery-lock").hide();
+
+                // Show the empty 'select new' portion of the slot.
+                $("#mastery-container-4" + " .character-sheet-mastery-empty").show();
+
+                // ...also play the flashy animation thingy.
+                $("#mastery-container-4" + " .mastery-change-flash").css("animation-play-state", "running");
+            }
+
+            // If rank is now maxed out, make it so we can't click on it anymore.
+            if(data.max)
+            {
+                $("#mastery-container-" + data.masterySlotIndex + " .character-sheet-mastery-icon-container")
+                    .removeAttr('href')
+                    .removeAttr('data-xf-click')
+                    .removeAttr('data-cache')
+                    .changeElementType("div");
+            }
+        }
+    });
+
+    // First element same as "data-xf-init" attribute
+    XF.Element.register('upgrade-mastery', 'XF.TS_CM_UpgradeMastery');
+}
+(jQuery, window, document);
