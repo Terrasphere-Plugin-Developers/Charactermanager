e39a83938ae5ec2bd43ce96a535a0d758e94bb5f
Skyswimsky <skyswim07@gmail.com>
2021-08-16 14:26:59 +0200

    Updated Character API route to contain nearly all information required(Equip/Mastery)

--- Api/Controller/Character.php
+++ Api/Controller/Character.php
@@ -2,48 +2,54 @@
 
 namespace Terrasphere\Charactermanager\Api\Controller;
 
-use Terrasphere\Charactermanager\Entity\CharacterMastery;
 use XF\Api\Controller\AbstractController;
 use XF\Mvc\ParameterBag;
-use XF\Mvc\Reply\AbstractReply;
-use XF\Mvc\RouteMatch;
+use XF\Api\Mvc\Reply\ApiResult;
 
-use XF\Mvc\Entity\Entity;
-
-class Character extends \XF\Api\Controller\AbstractController{
+class Character extends AbstractController{
 
 
-    public function actionGet(): \XF\Api\Mvc\Reply\ApiResult
+    /**
+     * Default Get Action for the Character Controller, requiring a user_id/name to function.
+     * @param ParameterBag $params
+     * @return ApiResult
+     */
+    public function actionGet(ParameterBag $params): ApiResult
     {
 
-
-
-        $options = $this->options();
-
-        $key = \XF::apiKey();
-
-        return $this->apiResult([
-            'version_id' => \XF::$versionId,
-            'key' => [
-                'type' => $key->key_type,
-                'allow_all_scopes' => $key->allow_all_scopes,
-                'scopes' => $key->scopes
-            ]
-        ]);
-    }
-
-
-    public function actionGetMasteries(){
-        $this->assertRequiredApiInput('username');
-        $username=$this->filter('username','str');
-
+        ///Check if the user ID is numeric or a name, if numeric it's the user id, if name it is the user name
         /** @var \Terrasphere\Charactermanager\XF\Entity\User $user */
-        $user=$this->finder('XF:User')->where('username',$username)->fetchOne();
+        $userId = $params->user_id;
+        if(is_numeric($userId)){
+            $user=$this->finder('XF:User')->where('user_id',$userId)->fetchOne();
+        }else{
+            $user=$this->finder('XF:User')->where('username',$userId)->fetchOne();
+        }
 
+
+        //Build proper equipment array to return apiresult
+        //TODO: Need to refactor how equipment works for this to work
+        $equipments = $this->finder('Terrasphere\Charactermanager:Equipment')->where('user_id', $user->user_id);
+        $equipResult = array();
+        foreach($equipments as $equipment){
+            $equipName = $user->getDisplayNameForEquip($equipment->type);
+            $equipRank = $equipment->Rank->name;
+            $equipResult[] = [$equipName => $equipRank];
+        }
+
+        //Build the prooper mastery array to return
         $masteries = $user->getMasteries();
-        $result = $masteries[0];
+        $masteryResult = array();
+        foreach($masteries as $mastery){
+            $masteryName = $mastery->Mastery->display_name;
+            $masteryRank = $mastery->Rank->name;
+            $masteryResult[] = ['Mastery' => $masteryName, 'Rank' => $masteryRank];
+        }
+
         return $this->apiResult([
-            'val' => $masteries
+            'user' => $user->user_id,
+            'masteries' => $masteryResult,
+            'equipment' => $equipResult,
         ]);
     }
 
